c696c1321754cb3fcfc00e7fe1115f95fe4c98f0
achyuth reddy <ncsachyuth@gmail.com>
2020-08-15 20:37:35 +0530

    password is updated

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const Schema  = mongoose.Schema;
+const crypto = require('crypto');
+const { v4: uuidv4 } = require('uuid');
 
 const userSchema = new Schema({
     name : {
@@ -23,10 +25,10 @@ const userSchema = new Schema({
         type: String,
         trim : true
     },
-    //  TODO come back here
-    password : {
+    //  TODO: password should be hashed!!
+    encrypted_password : {
         type: String,
-        trim : true
+        required:true
     },
     salt : String,
     role : {
@@ -38,7 +40,37 @@ const userSchema = new Schema({
         default : []
     },
     
-})
+});
+
+userSchema.virtual("password")
+    .set( function (password){
+        this._password = password
+        this.salt = uuidv1();
+        this.encrypted_password = this.securePassword(password)
+    } )
+    .get( function(){
+        return this._password
+    })
+
+
+
+userSchema.method = {
+    
+    authenticate:function (plain_password){
+        return this.securePassword(plain_password) === this.encrypted_password
+    },
+
+    securePassword : function(plain_password){
+        if (!plain_password) return "";
+        try {
+            return crypto.createHmac('sha256', this.salt)
+            .update(plain_password)
+            .digest('hex')            
+        } catch (error) {
+            return "";            
+        }
+    }
+}
 
 
 module.exports = mongoose.model("User" , userSchema);
