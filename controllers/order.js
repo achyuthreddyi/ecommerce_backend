a4a5417a94965aacab0d1a9b240cab726237ff1d
achyuth reddy <ncsachyuth@gmail.com>
2020-08-20 03:53:27 +0530

    order status is changed according to the enum values

--- controllers/order.js
+++ controllers/order.js
@@ -26,5 +26,37 @@ exports.createNewOrder = (req,res) =>{
         }
         res.json(order)
     })
+}
 
+exports.getAllOrders = (req, res) =>{
+    Order.find()
+        .populate("user" , "_id name")
+        .exec( (error,order) =>{
+            if(error){
+                return res.status(400).json({
+                    error: "CANNOT CREATE ORDER"
+                })
+            }
+            res.json(order)
+        })
+}
+
+exports.getOrderStatus = (req,res) =>{
+    res.json(Order.schema.path("status").enumValues)
+
+}
+
+exports.updateStatus = (req,res) =>{
+    Order.update(
+        {_id: req.body.orderId},
+        {$set : {status : req.body.status}},
+        (error,order) =>{
+            if(error){
+                return res.status(400).json({
+                    error: "STATUS CANNOT BE UPDATED !!"
+                })
+            }
+            res.json(order)
+        } 
+        )
 }
--- routes/order.js
+++ routes/order.js
@@ -4,7 +4,7 @@ const { isSignedIn,isAuthenticated,isAdmin } = require("../controllers/auth");
 const { getUserById,pushOrderInPurchaseList } = require("../controllers/user");
 const { updateStock } = require("../controllers/product")
 
-const { getOrderById,createNewOrder } = require("../controllers/order")
+const { getOrderById,createNewOrder,getAllOrders,getOrderStatus,updateStatus } = require("../controllers/order")
 
 //router params
 router.param("userId",getUserById)
@@ -20,6 +20,30 @@ router.post(
     updateStock,
     createNewOrder)
 
-// read
+// read the routes that has been recieved
+router.get(
+    "/order/all/:userId",
+    isSignedIn,
+    isAuthenticated,
+    isAdmin,
+    getAllOrders
+)
+
+//status of order
+router.get(
+    "/order/status/:userId",
+    isSignedIn,
+    isAuthenticated,
+    isAdmin,
+    getOrderStatus
+)
+
+router.put(
+    "/order/:orderId/status/:userId",
+    isSignedIn,
+    isAuthenticated,
+    isAdmin,
+    updateStatus
+    );
 
 module.exports = router;
